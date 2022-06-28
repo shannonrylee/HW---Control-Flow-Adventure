aa2fc0694743697a230d260af674c826613f5293
SHA256:hOXSm53Eq3p5Z4sDLk3h/LjyDO3EhgoJf0cCmjpAP04 <SHA256:hOXSm53Eq3p5Z4sDLk3h/LjyDO3EhgoJf0cCmjpAP04>
2022-06-28 18:12:42 -0400

    make changes to index.html

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ let gameStart = prompt(`Hi! Would you like to go on an adventure?`);
 if (gameStart === `yes`) {
   alert(`Great! Lets go!`);
 } else if (gameStart === `no`) {
-  alert(`Ok, maybe next time!`);
+  alert(`Ok, maybe next time!`)=null;
+  console.log(gameStart);
 }
-gameStart();
 
 let destinationOne = prompt(`Great! A nice hike or walk up the beach?`);
 if (destinationOne === `hike`) {
@@ -15,16 +15,16 @@ if (destinationOne === `hike`) {
   alert(
     `Fun in the sun! It's a hot one today, we should stop to get some snacks`
   );
+  console.log(destinationOne);
 }
-destinationOne();
 
-let destinationTwo = prompt(`Where should we stop 711 or Wawa?`);
+let destinationTwo = prompt(`Where should we stop 711 or wawa?`);
 if (destinationTwo === `711`) {
   alert(`711 it is!`);
-} else if (destinationTwo === `Wawa`) {
+} else if (destinationTwo === `wawa`) {
   alert(`Wawa it is!`);
+  console.log(destinationTwo);
 }
-destinationTwo();
 
 let destinationThree = prompt(
   `Ugh, we forgot waters. Target and Walmart are on the way, which do you prefer?`
@@ -33,18 +33,18 @@ if (destinationThree === `target`) {
   alert(`Target is the best, lets go`);
 } else if (destinationThree === `walmart`) {
   alert(`Walmart is the best, lets go`);
+  console.log(destinationThree);
 }
-destinationThree();
 
 let destinationFour = prompt(
-  `You're not going to believe this... No one has been home all day, I have to go home and walk the dogs. Since we are here I want to pick them up a toy first. Pupperonis or Milkbones?`
+  `You're not going to believe this... No one has been home all day, I have to go home and walk the dogs. Since we are here I want to pick them up a toy first. pupperonis or milkbones?`
 );
 if (destinationFour === `pupperonis`) {
   alert(`My dogs love pupperonis`);
 } else if (destinationFour === `milkbones`) {
   alert(`My dogs love milkbones`);
+  console.log(destinationFour);
 }
-destinationFour();
 
 let destinationFive = prompt(
   `Do you want to walk the dogs with me? yes or no?`
@@ -52,9 +52,9 @@ let destinationFive = prompt(
 if (destinationFive === "yes") {
   alert(`Awsome, thanks!`);
 } else if (destinationFive === `no`) {
-  alert(`It's cool, i'll be back in a bit`);
+  alert(`It's cool, i'll be back in a bit`)=null;
+  console.log(destinationFive);
 }
-destinationFive();
 
 let destinationSix = prompt(
   `Ok now that this barbie playhouse video game of a situation is over. Want to head over there now, yes or no?`
@@ -64,16 +64,16 @@ if (destinationSix === `yes`) {
 } else if (destinationSix === `no thanks`) {
   alert(`Yeah it did kind of take long, we'll go another day.`);
 } else {
-  alert(`Yeah, I get it. I'm going to take a nap.`);
+  alert(`Yeah, I get it. I'm going to take a nap.`)=null;
+  console.log(destinationSix);
 }
-destinationSix();
 
 let destinationSeven = prompt(
   `I am so glad we made it, on the scale from 1-10. How gorgeous is this view?`
 );
-for (let i = 0; i < 11, i++; ) {
-  console.log(destinationSeven[i]);
+for (let i = 0; i <= 11; ) {
   alert(`Ah! Same!`);
+  console.log(destinationSeven);
 }
 
 // need help switch & repeat.
